42b9badb1d809c0d0c7fc3b12c1e231d412113fb
ankit kumar <ankitsarwal7@gmail.com>
2024-12-13 21:00:34 +0530

    auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -163,7 +163,34 @@ return res
 
 
 const logoutUser = asyncHandler(async(req, res) => {
-      
+     await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+
+    )
+
+const options = {
+    httpOnly: true,
+    secure:true
+}
+ return res
+ .status(200)
+ .clearCookie("accessToken", options)
+ .clearCookie("refreshToken", options)
+ .json( new ApiResponse(200, {}, "user logged out"))
+
+
+
+
+
+
 })
 
 
@@ -205,7 +232,8 @@ const logoutUser = asyncHandler(async(req, res) => {
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 } 
 
 
